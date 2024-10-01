0171feb9a22c0f8894fd2998c8fb199ca92fc9e3
irfanshafee <irfanshafee1@gmail.com>
2024-10-01 12:33:35 +0600

    added all the classes and other files completetly

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BookListTracker tracker = new BookListTracker();
-        // tracker.loadBooks("C://Users//irfan//Desktop//OOC-2 lab//LAB - 4//Lab4//Book.xml", "xml");
+         tracker.loadBooks("C://Users//irfan//Desktop//OOC-2 lab//LAB - 4//Lab4//Book.xml", "xml");
         //tracker.loadBooks("C://Users//irfan//Desktop//OOC-2 lab//LAB - 4//Lab4//Books.csv", "csv");
-         tracker.loadBooks("C://Users//irfan//Desktop//OOC-2 lab//LAB - 4//Lab4//Book.json", "json");
+         //tracker.loadBooks("C://Users//irfan//Desktop//OOC-2 lab//LAB - 4//Lab4//Book.json", "json");
 
         System.out.println("Total books read in the last year: " + tracker.totalBooksLastYear());
         System.out.println("Books read per month: " + tracker.booksPerMonth());
